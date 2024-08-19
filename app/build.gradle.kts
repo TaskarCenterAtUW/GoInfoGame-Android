aa9ad75797d86607089334297f5ce280aa8c650d
Rajesh Kantipudi <iamrajeshkantipudi@gmail.com>
2024-08-19 16:21:10 +0530

    Updated version number to 3, removed settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.tcatuw.goinfo"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
