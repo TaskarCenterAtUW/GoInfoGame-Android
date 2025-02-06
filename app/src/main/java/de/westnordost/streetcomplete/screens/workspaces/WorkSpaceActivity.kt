dfd59007f2a142dbac855824ea6369b1fd6f2ef4
rajeshkantipudi <iamrajeshkantipudi@gmail.com>
2025-02-06 21:36:12 +0530

    Fixed issues with location services

--- app/src/main/java/de/westnordost/streetcomplete/screens/workspaces/WorkSpaceActivity.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/workspaces/WorkSpaceActivity.kt
@@ -1,14 +1,19 @@
 package de.westnordost.streetcomplete.screens.workspaces
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,20 +22,25 @@ import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
@@ -47,12 +57,15 @@ class WorkSpaceActivity : ComponentActivity() {
 
     private val preferences: Preferences by inject()
 
+    private val _isLocationEnabled = mutableStateOf(false)
+    private val isLocationEnabled: State<Boolean> get() = _isLocationEnabled
+
     private val requestLocationPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted: Boolean ->
         if (isGranted) {
             // Permission is granted, proceed with location access
-        checkLocationPermission()
+            checkLocationPermission()
         }
     }
 
@@ -60,7 +73,56 @@ class WorkSpaceActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        checkLocationPermission()
+        isLocationEnabled()
+
+        // Check if location is enabled
+        setContent()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isLocationEnabled()
+    }
+
+    private fun setContent() {
+        if (isLocationEnabled.value) {
+            checkLocationPermission()
+        } else {
+            setContent {
+                AppTheme {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text(
+                            "GoInfoGame needs location to be enabled to fetch workspaces nearby",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(16.dp),
+                            style = TextStyle(fontSize = 20.sp)
+                        )
+                        if (!isLocationEnabled.value) {
+                            Button(onClick = {
+                                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                                startActivity(intent)
+                            }) {
+                                Text("Enable location")
+                            }
+                        }
+                        Button(onClick = { setContent() }) {
+                            Text("Refresh")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun isLocationEnabled() {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        _isLocationEnabled.value =
+            locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
     @OptIn(ExperimentalMaterial3Api::class)
@@ -71,33 +133,40 @@ class WorkSpaceActivity : ComponentActivity() {
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED -> {
                 // Permission is already granted
-                setContent { AppTheme {
-                    val workspaceLoginState by preferences.workspaceLoginState.collectAsState()
-                    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-                        TopAppBar(
-                            title = { Text(text = "Go Info Game") },
-                            actions = {
-                                if (workspaceLoginState) {
-                                    Icon(
-                                        imageVector = Icons.Default.Person, // Replace with your desired icon
-                                        contentDescription = "Star Icon",
-                                        modifier = Modifier.padding(end = 16.dp).clickable {
-                                            val intent = Intent(this@WorkSpaceActivity, UserActivity::class.java)
-                                            startActivity(intent)
-                                        }
-                                    )
-                                }
-                            },
-                            modifier = Modifier.wrapContentSize(Alignment.Center),
-                        )
-                    }, contentWindowInsets = WindowInsets.statusBars) { innerPadding ->
-                        // LoginScreen(
-                        //     viewModel = koinViewModel(),
-                        //     modifier = Modifier.padding(innerPadding)
-                        // )
-                        AppNavigator(innerPadding, preferences)
+                setContent {
+                    AppTheme {
+                        val workspaceLoginState by preferences.workspaceLoginState.collectAsState()
+                        Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                            TopAppBar(
+                                title = { Text(text = "Go Info Game") },
+                                actions = {
+                                    if (workspaceLoginState) {
+                                        Icon(
+                                            imageVector = Icons.Default.Person, // Replace with your desired icon
+                                            contentDescription = "Star Icon",
+                                            modifier = Modifier
+                                                .padding(end = 16.dp)
+                                                .clickable {
+                                                    val intent = Intent(
+                                                        this@WorkSpaceActivity,
+                                                        UserActivity::class.java
+                                                    )
+                                                    startActivity(intent)
+                                                }
+                                        )
+                                    }
+                                },
+                                modifier = Modifier.wrapContentSize(Alignment.Center),
+                            )
+                        }, contentWindowInsets = WindowInsets.statusBars) { innerPadding ->
+                            // LoginScreen(
+                            //     viewModel = koinViewModel(),
+                            //     modifier = Modifier.padding(innerPadding)
+                            // )
+                            AppNavigator(innerPadding, preferences)
+                        }
                     }
-                } }
+                }
             }
 
             else -> {
