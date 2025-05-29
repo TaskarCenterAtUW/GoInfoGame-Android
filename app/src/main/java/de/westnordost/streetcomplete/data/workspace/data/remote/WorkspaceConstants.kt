f5829ca1608bbe4723ce86e5f3ae0ad2679aaf04
Rajesh K <iamrajeshkantipudi@gmail.com>
2025-05-29 19:54:02 +0530

    Set default environment to PROD
    The default environment in `EnvironmentManager` is now set to `PROD` instead of `STAGE`.

--- app/src/main/java/de/westnordost/streetcomplete/data/workspace/data/remote/WorkspaceConstants.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/workspace/data/remote/WorkspaceConstants.kt
@@ -29,7 +29,7 @@ enum class Environment(
 }
 
 class EnvironmentManager(val preferences: Preferences) {
-    var currentEnvironment: Environment = Environment.STAGE
+    var currentEnvironment: Environment = Environment.PROD
         get() {
             val name = preferences.environment
             return Environment.valueOf(name)
