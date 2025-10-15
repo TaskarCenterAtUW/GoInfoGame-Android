e531d918a58b4162e7b3c08434ae207d7692fd10
Rajesh Kantipudi <44539669+iamrajeshk@users.noreply.github.com>
2025-10-15 14:23:10 +0530

    Bump version code to 4 and version name to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "net.opentoall.aviv.scoutroute"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
