7a4a8e51a49a1d06c83c191767a6d207edd28479
Rajesh Kantipudi <44539669+iamrajeshk@users.noreply.github.com>
2026-01-15 23:18:50 +0530

    Increment app version to 1.1.3 and update version code to 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ import java.util.Properties
 
 
 /** App version name, code and flavor */
-val appVersionName = "1.1.2"
-val appVersionCode = 8
+val appVersionName = "1.1.3"
+val appVersionCode = 9
 
 /** Localizations the app should be available in */
 val bcp47ExportLanguages = setOf(
